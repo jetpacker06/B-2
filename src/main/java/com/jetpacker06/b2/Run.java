1353fe79681eac6e933c460c973f0f78cf1f84a1
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-08-08 12:06:04 -0500

    Initial commit

---
+++ src/main/java/com/jetpacker06/b2/Run.java
@@ -0,0 +1,10 @@
+package com.jetpacker06.b2;
+
+public class Run {
+    public static void main(String[] args) {
+        log("It's alive!");
+    }
+    public static void log(Object o) {
+        System.out.println(o);
+    }
+}
